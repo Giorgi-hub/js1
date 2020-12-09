d1b1ba30e1a21d9d8cea14b728587df140bdf536
Giorgi-hub <74665134+Giorgi-hub@users.noreply.github.com>
2020-12-09 18:59:17 +0400

    main

---
+++ main.js
@@ -0,0 +1,40 @@
+// //დავალება [1]:
+// //მოცემულია მასივი:
+//  let cars = ["audi","bmw", "lexus", "volkswagen", "ferrary", "porsche"];
+// // ციკლის დახმარებით, ეკრანზე გამოიტანეთ ყველა ელემენტი, დაწყებული მეორე ელემენტისგან მე-5ის ჩათვლით; 
+//  for(i=1; i<5; i++){
+//      console.log(cars[i])
+//  }
+// //დავალება [2]:
+// // შექმენით სახელების მასივი და დაბეჭდეთ ის სახელები რომლისს სიმბოლების რაოდენობა 3 ზე მეტია;
+//  let names = ["erti","ori","sami","otxi","tutxmeti"];
+//  for(let i=0; i<names.length; i++)
+//   if(names[i].length>3) {console.log(names[i])};
+// //დავალება [3]:
+// // შექმენით ციფრების მასივი და გამოიტანეთ მარტო ლუწი რიცხვები და მარტო კენტი რიცხვები;
+//   let arr = [392,10512,8381,99913,4123512,9999];
+//  for(let i=0; i<numbs.length; i++)
+//    if(numbs[i]%2==0) {console.log(numbs[i])};
+//  for(let i=0; i<numbs.length; i++)
+//    if(numbs[i]%2==1) {console.log(numbs[i])};
+// //დავალება [4]:
+// // მოცემულია მასივი:
+//  let user = ['name', 'age', 25, 50, "surname", [100, 500] ];
+// // ჩადგმული მასივიდან ამოიღეთ მეორე ელემენტი; ანუ 500;
+//  console.log(user[5][1])
+//  // დავალება [5]:
+//  // მოცემულია ობიექტი:
+//  var person = {
+//      firstname: "john",
+//      lastname: "doe",
+//      age: 50,
+//      eyecolor: "blue"
+//    };
+// // ამოიღეთ person ცვლადის თვალის ფერი;
+//  console.log(person.eyecolor);
+// // დავალება [6]:
+// // შექმენით მასივი, და დაბეჭდეთ სათითაოდ თითოეული ელემენტი;
+let name = [20,19,18,17,16,"tutxmeti"]
+for (let w = 0; w <name.lenght; w++ ) {
+    console.log(name[w])
+}
